02968414525209b5b76dfa45001b6461aa800859
amirito <amirmousavi_90@yahoo.com>
2022-12-13 17:57:29 +0300

    production environment added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# analytics
-analitycs SDK 
+# Analytics
+Game analitycs SDK
+
+## Installation
+
+Use this command:
+```js
+npm i @ideacompiler/analytics
+```
+
+## USAGE
+1. import module
+```js
+import Analytics from 'analytics/src/config'; 
+```
+
+2. init Analytics
+```js
+const myAnalytics = new Scuti($environment);
+```
+> * **$environment** can be one of ( '**staging**' or '**production**')
+
+3. Use `gameLog` method to log events
+```js
+myAnalytics.gameLog('myEventName', {...options })
+``` 
--- src/config.js
+++ src/config.js
@@ -31,20 +31,20 @@ import { getAnalytics, logEvent } from "firebase/analytics";
 
 export default class ScutiAnalytics {
 
-    constructor(isProduction) {
+    constructor(environment) {
 
         let firebaseConfig
-        if (isProduction) {
+        if (environment == 'production') {
             firebaseConfig = {
-                apiKey: "AIzaSyDPZQyi8s1OFpWBA0JwCnACA_XajF-DKmc",
-                authDomain: "xpf1-ec213.firebaseapp.com",
-                projectId: "xpf1-ec213",
-                storageBucket: "xpf1-ec213.appspot.com",
-                messagingSenderId: "381735665169",
-                appId: "1:381735665169:web:c4a2bdbab56a23c0643bb0",
-                measurementId: "G-5X51G25D6G"
+                apiKey: "AIzaSyCBo_3Q7EDTZMSp0LBrZQ9gipbPLXSxJBY",
+                authDomain: "prod-xpf1.firebaseapp.com",
+                projectId: "prod-xpf1",
+                storageBucket: "prod-xpf1.appspot.com",
+                messagingSenderId: "838293689075",
+                appId: "1:838293689075:web:aa203391ea2e64aa295d86",
+                measurementId: "G-BV7E8PBG4G"
             };
-        } else {
+        } else if (environment == 'staging') {
             firebaseConfig = {
                 apiKey: "AIzaSyDPZQyi8s1OFpWBA0JwCnACA_XajF-DKmc",
                 authDomain: "xpf1-ec213.firebaseapp.com",
@@ -65,14 +65,6 @@ export default class ScutiAnalytics {
 
     gameLog(eventName, properties) {
         logEvent(this.analytics, `GAME-${eventName}`, {
-            // gameId: properties.gameId,
-            // offerId: properties.offerId,
-            // sessionId: properties.sessionId,
-            // duration: properties.duration,
-            // claims: properties.claims,
-            // platform: properties.platform,
-            // device: properties.device,
-            // customData: properties.customData,
             ...properties
         });
     }
